a350aaa32d332cef047525563d9cb59d3ead3111
alensaito1 <erwinsmithscout1@gmail.com>
2021-11-04 19:22:39 +0530

    config(eslint): update rc

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,11 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+        "@typescript-eslint/no-unused-vars": [
+            "warn",
+            {
+                "varsIgnorePattern": "^h$",
+            }
+        ],
     }
 };
